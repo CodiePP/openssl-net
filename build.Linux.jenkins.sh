21c5c71600d4c14a32f332d6c4bc6dcb20b752d7
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-04 15:56:19 +0200

    jenkins job

--- build.Linux.jenkins.sh
+++ build.Linux.jenkins.sh
@@ -9,7 +9,6 @@ nuget restore -PackagesDirectory packages test/packages.config
 sn -k ManagedOpenSsl/openssl-net.snk
 
 xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" openssl.net.Linux.sln
-
 xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" openssl.net.Linux.sln
 
 packages/NUnit.Runners.lite.2.6.4.20150512/nunit-console.exe -labels bin/Debug/UnitTests.dll
